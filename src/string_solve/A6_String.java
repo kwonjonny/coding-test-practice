d341631ce0fc1cd17978937753cc7b3f7bc4f339
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-04 14:56:05 +0900

    Feat: 중복 문자 제거
    - 문자열 중 중복된 문자를 제거하는 기능
    
    문자열 중 중복된 문자를 찾아내 소문자로 변환 후 중복을 제거하는 기능

---
+++ src/string_solve/A6_String.java
@@ -0,0 +1,48 @@
+package string_solve;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+/**
+ * 중복 문자 제거
+ *
+ * 설명: 소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하세요.
+ * 중복이 제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+ *
+ * 조건:
+ * 1. 첫 줄에 문자열이 입력됩니다. 문자열의 길이는 100을 넘지 않는다.
+ *
+ * 출력:
+ * - 첫 줄에 중복문자가 제거된 문자열을 출력합니다.
+ *
+ * 예제:
+ * 입력:
+ *  ksekkset
+ *
+ * 출력:
+ *   kset
+ */
+public class A6_String {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        String solution = solution(str);
+        System.out.println(solution);
+    }
+
+    public static String solution(String str) {
+        LinkedHashSet<Character> linkedHashSet = new LinkedHashSet<>();
+
+        for(char x : str.toCharArray()) {
+            char t = Character.toLowerCase(x);
+            linkedHashSet.add(t);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(char x : linkedHashSet) {
+            stringBuilder.append(x);
+        }
+        return stringBuilder.toString();
+    }
+}
