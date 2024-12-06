5c3c42cff5d72b6b72783601db7be8fdb3b6b112
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-06 13:19:37 +0900

    Feat: 유효한 팰린드롬 재 복습
    - 유효한 팰린드롬 재 복습

--- src/string_solve/A8_String.java
+++ src/string_solve/A8_String.java
@@ -1,6 +1,7 @@
 package string_solve;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -55,5 +56,73 @@ public class A8_String {
             right--;
         }
         return "YES";
+    }
+
+    /**
+     * 2024-12-06 복습
+     */
+    public static class A8String {
+        public static void main(String[] args) {
+            A8String a8String = new A8String();
+            Scanner scan = new Scanner(System.in);
+            String str = scan.nextLine();
+            String solution = a8String.solution(str);
+            System.out.println(solution);
+        }
+
+        public String solution(String str) {
+            List<Character> list = new LinkedList<>();
+            for(char x : str.toCharArray()) {
+                if(Character.isLetter(x)) {
+                    list.add(Character.toUpperCase(x));
+                }
+            }
+
+            Integer left = 0;
+            Integer right = list.size() - 1;
+
+            while (left > right) {
+                if(!list.get(left).equals(list.get(right))) {
+                    return "NO";
+                }
+                left++;
+                right--;
+            }
+            return "YES";
+        }
+    }
+
+    /**
+     * 2024-12-06 복습
+     */
+    public static class A8String1 {
+        public static void main(String[] args) {
+            A8String1 a8String1 = new A8String1();
+            Scanner scan = new Scanner(System.in);
+            String str = scan.nextLine();
+            String solution = a8String1.solution(str);
+            System.out.println(solution);
+        }
+
+        public String solution(String str) {
+            LinkedList<Character> linkedList = new LinkedList<>();
+            for(char x : str.toCharArray()) {
+                if(Character.isLetter(x)) {
+                    linkedList.add(x);
+                }
+            }
+
+            Integer left = 0;
+            Integer right = linkedList.size() - 1;
+
+            while (left > right) {
+                if(!linkedList.get(left).equals(linkedList.get(right))) {
+                    return "NO";
+                }
+                left ++ ;
+                right -- ;
+            }
+            return "YES";
+        }
     }
 }
