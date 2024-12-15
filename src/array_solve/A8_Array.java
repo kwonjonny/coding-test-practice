a0b8badf7028e22c9d96c227a9b6064d5ad3e832
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-15 21:10:10 +0900

    Feat: 등수구하기
    - N명의 학생의 국어점수가 입력되면 각 학생의 등수를 출력하는 기능

---
+++ src/array_solve/A8_Array.java
@@ -0,0 +1,54 @@
+package array_solve;
+
+import java.util.Scanner;
+
+/**
+ *  등수구하기
+ *
+ * 설명:
+ * N명의 학생의 국어점수가 입력되면 각 학생의 등수를 입력된 순서대로 출력하는 프로그램을 작성하세요.
+ *
+ * 같은 점수가 입력될 경우 높은 등수로 동일 처리한다.
+ *
+ * 즉 가장 높은 점수가 92점인데 92점이 3명 존재하면 1등이 3명이고 그 다음 학생은 4등이 된다.
+ *
+ * 입력:
+ *  - 첫 줄에 N(3<=N<=100)이 입력되고, 두 번째 줄에 국어점수를 의미하는 N개의 정수가 입력된다.
+ *
+ * 출력:
+ * - 입력된 순서대로 등수를 출력한다.
+ *
+ * 예제:
+ * 입력:
+ *  5
+ *  87 89 92 100 76
+ *
+ * 출력:
+ *  4 3 2 1 5
+ */
+public class A8_Array {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Integer count = scan.nextInt();
+        Integer[] intArray = new Integer[count];
+        for(int i = 0; i < count; i++) {
+            intArray[i] = scan.nextInt();
+        }
+        for(int x : solution(count, intArray)) {
+            System.out.print(x + " ");
+        }
+    }
+    public static int[] solution(Integer count, Integer[] intArray) {
+        int[] answer = new int[count];
+        for(int i = 0; i < count; i++) {
+            int cnt = 1;
+            for(int j = 0; j < count; j++) {
+                if(intArray[i] < intArray[j]) {
+                    cnt++;
+                }
+                answer[i] = cnt;
+            }
+        }
+        return answer;
+    }
+}
