c4e82e9acf202b548819bdeadd088bada8fb963b
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-21 19:24:51 +0900

    Feat: 최대 매출
    Feat: 최대 매출

---
+++ src/two_pointers/A_SlidingWindow3.java
@@ -0,0 +1,67 @@
+package two_pointers;
+
+import java.util.Scanner;
+
+/**
+ * 최대 매출
+ * 설명
+ *
+ * 현수의 아빠는 제과점을 운영합니다. 현수 아빠는 현수에게 N일 동안의 매출기록을 주고 연속된 K일 동안의 최대 매출액이 얼마인지 구하라고 했습니다.
+ *
+ * 만약 N=10이고 10일 간의 매출기록이 아래와 같습니다. 이때 K=3이면
+ *
+ * 12 1511 20 2510 20 19 13 15
+ *
+ * 연속된 3일간의 최대 매출액은 11+20+25=56만원입니다.
+ *
+ * 여러분이 현수를 도와주세요.
+ *
+ *
+ * 입력
+ *
+ * 첫 줄에 N(5<=N<=100,000)과 K(2<=K<=N)가 주어집니다.
+ *
+ * 두 번째 줄에 N개의 숫자열이 주어집니다. 각 숫자는 500이하의 음이 아닌 정수입니다.
+ *
+ *
+ * 출력
+ *
+ * 첫 줄에 최대 매출액을 출력합니다.
+ *
+ *
+ * 예시 입력
+ *
+ * 10 3
+ * 12 15 11 20 25 10 20 19 13 15
+ *
+ * 예시 출력
+ * 56
+ */
+public class A_SlidingWindow3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Integer length = scan.nextInt();
+        Integer sliding = scan.nextInt();
+        Integer[] array = new Integer[length];
+        for(int i = 0; i < length; i++) {
+            array[i] = scan.nextInt();
+        }
+        System.out.println(solution(length, sliding, array));
+    }
+
+    public static Integer solution(Integer length, Integer sliding, Integer[] array) {
+        int max = 0;
+        int currentSum = 0;
+
+        for(int i = 0; i < sliding; i++) {
+            currentSum = currentSum + array[i];
+        }
+        max = currentSum;
+
+        for(int i = sliding; i < length; i++) {
+            currentSum = currentSum + (array[i] - array[i - sliding]);
+            max = Math.max(max, currentSum);
+        }
+        return max;
+    }
+}
