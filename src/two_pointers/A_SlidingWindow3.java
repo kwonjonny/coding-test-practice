03bc3d932ea26c127efc30126277858e0f1d54b6
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-25 19:10:59 +0900

    Feat: 최대 매출
    Feat: 최대 매출

--- src/two_pointers/A_SlidingWindow3.java
+++ src/two_pointers/A_SlidingWindow3.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * 만약 N=10이고 10일 간의 매출기록이 아래와 같습니다. 이때 K=3이면
  *
- * 12 15 11 20 2510 20 19 13 15
+ * 12 15 11 20 25 10 20 19 13 15
  *
  * 연속된 3일간의 최대 매출액은 11+20+25=56만원입니다.
  *
@@ -31,8 +31,8 @@ import java.util.Scanner;
  *
  * 예시 입력
  *
- * 10 3
- * 12 15 11 20 25 10 20 19 13 15
+   10 3
+   12 15 11 20 25 10 20 19 13 15
  *
  * 예시 출력
  * 56
@@ -92,6 +92,38 @@ public class A_SlidingWindow3 {
             for(int i = days; i < length; i++) {
                 currentSum = currentSum + (intArray[i] - intArray[i - days]);
                 max = Math.max(max, currentSum);
+            }
+            return max;
+        }
+    }
+
+    /**
+     * 재 복습: 2024-12-25
+     */
+    public static class ASlidingWindow4 {
+        public static void main(String[] args) {
+            ASlidingWindow4 aSlidingWindow4 = new ASlidingWindow4();
+            Scanner scan = new Scanner(System.in);
+            int length = scan.nextInt();
+            int sliding = scan.nextInt();
+            int[] array = new int[length];
+            for(int i = 0; i < length; i++) {
+                array[i] = scan.nextInt();
+            }
+            System.out.println(aSlidingWindow4.solution(length, sliding, array));
+        }
+        public Integer solution(int length, int sliding, int[] array) {
+            int max = 0;
+            int sum = 0;
+
+            for(int i = 0; i < sliding; i++) {
+                sum = sum + array[i];
+            }
+            max = sum;
+
+            for(int i = sliding; i < length; i++) {
+                sum = sum + (array[i] - array[i - sliding]);
+                max = Math.max(max, sum);
             }
             return max;
         }
