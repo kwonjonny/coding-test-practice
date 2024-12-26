4b33d25ad9e6ddb6dda8106952417c153f54231f
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-26 10:28:04 +0900

    Feat: 최대 길이 연속부분수열
    Feat: 최대 길이 연속부분수열

--- src/two_pointers/A_TwoPointersAndSlidingWindow3.java
+++ src/two_pointers/A_TwoPointersAndSlidingWindow3.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
  * 최대 길이 연속부분수열
  * 설명
  *
- * 0과 1로 구성된 길이가 N인 수열이 주어집니다. 여러분은 이 수열에서 최대 k번을 0을 1로 변경할 수 있습니다. 여러분이 최대 k번의 변경을 통해 이 수열에서 1로만 구성된 최대 길이의 연속부분수열을 찾는 프로그램을 작성하세요.
+ * 0과 1로 구성된 길이가 N인 수열이 주어집니다. 여러분은 이 수열에서 최대 k번을 0을 1로 변경할 수 있습니다.
+ *
+ * 여러분이 최대 k번의 변경을 통해 이 수열에서 1로만 구성된 최대 길이의 연속부분수열을 찾는 프로그램을 작성하세요.
+ *
  * 만약 길이가 길이가 14인 다음과 같은 수열이 주어지고 k=2라면
  *
  * 1 1 0 0 1 1 0 1 1 0 1 1 0 1
@@ -26,6 +29,7 @@ import java.util.Scanner;
  * 8
  */
 public class A_TwoPointersAndSlidingWindow3 {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int length = scan.nextInt();
@@ -50,5 +54,37 @@ public class A_TwoPointersAndSlidingWindow3 {
             answer = Math.max(answer, right - left + 1);
         }
         return answer;
+    }
+
+    /**
+     * 재 복습: 2024-12-26
+     */
+    public static class ATwoPointersAndSlidingWindow3 {
+        public static void main(String[] args) {
+            ATwoPointersAndSlidingWindow3 aTwoPointersAndSlidingWindow3 = new ATwoPointersAndSlidingWindow3();
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer changeCount = scan.nextInt();
+            Integer[] intArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                intArray[i] = scan.nextInt();
+            }
+            System.out.println(aTwoPointersAndSlidingWindow3.solution(length, changeCount, intArray));
+        }
+        public Integer solution(Integer length , Integer changeCount, Integer[] intArray){
+            Integer answer = 0;
+            Integer zeroCount = 0;
+            Integer left = 0;
+            for(int right = 0; right < length; right++) {
+                if(intArray[right] == 0) zeroCount++;
+
+                while(zeroCount > changeCount) {
+                    if(intArray[left] == 0) zeroCount--;
+                    left++;
+                }
+                answer = Math.max(answer, right - left + 1);
+            }
+            return answer;
+        }
     }
 }
