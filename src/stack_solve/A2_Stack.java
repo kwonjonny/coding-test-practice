858c2247c2119cdc1fdf913126a96548ffb22a79
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-28 18:09:23 +0900

    Feat: 괄호 문자 제거 (Stack)
    Feat: 괄호 문자 제거 (Stack)

---
+++ src/stack_solve/A2_Stack.java
@@ -0,0 +1,47 @@
+package stack_solve;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * 괄호문자제거
+ * 설명
+ *
+ * 입력된 문자열에서 소괄호 ( ) 사이에 존재하는 모든 문자를 제거하고 남은 문자만 출력하는 프로그램을 작성하세요.
+ *
+ * 입력
+ * 첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
+ *
+ * 출력
+ * 남은 문자만 출력한다.
+ *
+ * 예시 입력
+    (A(BC)D)EF(G(H)(IJ)K)LM(N)
+ *
+ * 예시 출력
+ * EFLM
+ */
+public class A2_Stack {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        String answer = solution(str);
+        System.out.println(answer);
+    }
+    public static String solution(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (Character x : str.toCharArray()) {
+            if(x.equals(')')) {
+                while(stack.pop()!='(');
+            } else {
+                stack.push(x);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Character x : stack) {
+            stringBuilder.append(x);
+        }
+        return stringBuilder.toString();
+    }
+}
