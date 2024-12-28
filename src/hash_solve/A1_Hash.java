60d3f998c7feb067931abb6dc73aa512775c115a
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-28 17:37:24 +0900

    Feat: 학급 회장 (HashMap)
    Feat: 학급 회장 (HashMap)

--- src/hash_solve/A1_Hash.java
+++ src/hash_solve/A1_Hash.java
@@ -114,7 +114,7 @@ public class A1_Hash {
             return answer;
         }
     }
-    
+
     /**
      * 재 복습: 2024-12-28
      */
@@ -134,6 +134,35 @@ public class A1_Hash {
                 hashMap.put(x, hashMap.getOrDefault(x, 0) + 1);
             }
             int max = 0;
+            for(Map.Entry<Character, Integer> map : hashMap.entrySet()) {
+                if(map.getValue() > max) {
+                    max = map.getValue();
+                    answer = map.getKey();
+                }
+            }
+            return answer;
+        }
+    }
+
+    /**
+     * 재 복습: 2024-12-28
+     */
+    public static class A1Hash1 {
+        public static void main(String[] args) {
+            A1Hash1 a1Hash1 = new A1Hash1();
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            String str = scan.next();
+            Character answer = a1Hash1.solution(length, str);
+            System.out.println(answer);
+        }
+        public Character solution(Integer length, String str) {
+            Character answer = null;
+            HashMap<Character, Integer> hashMap = new HashMap<>();
+            for(Character x : str.toCharArray()) {
+                hashMap.put(x, hashMap.getOrDefault(x, 0) + 1);
+            }
+            Integer max = 0;
             for(Map.Entry<Character, Integer> map : hashMap.entrySet()) {
                 if(map.getValue() > max) {
                     max = map.getValue();
