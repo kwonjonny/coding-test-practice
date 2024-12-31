badfca70d612018302354074e04b4bf474715d46
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-31 20:46:29 +0900

    Feat: 봉우리
    Feat: 봉우리

--- src/array_solve/B1_Array.java
+++ src/array_solve/B1_Array.java
@@ -295,4 +295,45 @@ public class B1_Array {
             return answer;
         }
     }
+
+    /**
+     * 재 복습:2024-12-31
+     */
+    public static class B1Array6 {
+        public static void main(String[] args) {
+            B1Array6 b1Array6 = new B1Array6();
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer[][] intArray = new Integer[length][length];
+            for(int i = 0; i < length; i++) {
+                for(int j = 0; j < length; j++) {
+                    intArray[i][j] = scan.nextInt();
+                }
+            }
+            Integer answer = b1Array6.solution(length, intArray);
+            System.out.println(answer);
+        }
+        public Integer solution(Integer length, Integer[][] intArray) {
+            Integer answer = 0;
+            int[] dx = {-1, 0, 1, 0};
+            int[] dy = {0, 1, 0, -1};
+            for(int i = 0; i < length; i++) {
+                for(int j = 0; j < length; j++) {
+                    boolean isPeak = true;
+                    for(int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if(nx >= 0 && nx < length && ny >= 0 && ny < length) {
+                            if(intArray[i][j] < intArray[nx][ny]) {
+                                isPeak = false;
+                                break;
+                            }
+                        }
+                    }
+                    if(isPeak) answer++;
+                }
+            }
+            return answer;
+        }
+    }
 }
