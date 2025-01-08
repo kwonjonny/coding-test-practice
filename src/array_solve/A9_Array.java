7dc569a42a5a627e5e0fa0b79f80fb0870400b15
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-01-08 21:37:37 +0900

    Feat: 격차판 최대합 (2차원 Array)
    Feat: 격차판 최대합 (2차원 Array)

--- src/array_solve/A9_Array.java
+++ src/array_solve/A9_Array.java
@@ -362,4 +362,44 @@ public class A9_Array {
             return answer;
         }
     }
+
+    /**
+     * 재 복습: 2025-01-08
+     */
+    public static class A9Array7 {
+        public static void main(String[] args) {
+            A9Array7 a9Array7 = new A9Array7();
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer[][] intArray = new Integer[length][length];
+            for(int i = 0; i < length; i++) {
+                for(int j = 0; j < length; j++) {
+                    intArray[i][j] = scan.nextInt();
+                }
+            }
+            a9Array7.solution(length, intArray);
+        }
+        public void solution(Integer length, Integer[][] intArray) {
+            int max = 0;
+            for(int i = 0; i < length; i++) {
+                int sum1 = 0;
+                int sum2 = 0;
+                for(int j = 0; j < length; j++) {
+                    sum1 = sum1 + intArray[i][j];
+                    sum2 = sum2 + intArray[j][i];
+                }
+                max = Math.max(max, sum1);
+                max = Math.max(max, sum2);
+            }
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int i = 0; i < length; i++) {
+                sum1 = sum1 + intArray[i][i];
+                sum2 = sum2 + intArray[i][length - i - 1];
+            }
+            max = Math.max(max, sum1);
+            max = Math.max(max, sum2);
+            System.out.println(max);
+        }
+    }
 }
