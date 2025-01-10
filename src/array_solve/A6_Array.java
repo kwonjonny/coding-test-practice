a1778f65029f013ff645f5e0d7525ca19d5f0e71
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-01-10 23:42:33 +0900

    Feat: 뒤집은 소수 (Array)
    Feat: 뒤집은 소수 (Array)

--- src/array_solve/A6_Array.java
+++ src/array_solve/A6_Array.java
@@ -32,6 +32,46 @@ import java.util.*;
 public class A6_Array {
 
     /**
+     * 재 복습: 2025-01-10
+     */
+    public static class A6Array9 {
+        public static void main(String[] args) {
+            A6Array9 a6Array9 = new A6Array9();
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer[] intArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                intArray[i] = scan.nextInt();
+            }
+            a6Array9.solution(length, intArray);
+        }
+        public void solution(Integer length, Integer[] intArray) {
+            List<Integer> answer = new ArrayList<>();
+            for(int i = 0; i < length; i++) {
+                int temp = intArray[i];
+                int res = 0;
+                while(temp > 0) {
+                    int t = temp % 10;
+                    res = res * 10 + t;
+                    temp = temp / 10;
+                }
+                A6Array9 A = new A6Array9();
+                if(A.isPrime(res)) answer.add(res);
+            }
+            for(Integer x : answer) {
+                System.out.print(x + " ");
+            }
+        }
+        public Boolean isPrime(Integer num) {
+            if(num == 1) return false;
+            for(int i = 2; i < num; i++) {
+                if(num % i == 0) return false;
+            }
+            return true;
+        }
+    }
+
+    /**
      * 재 복습: 2025-01-09
      */
     public static class A6Array7 {
