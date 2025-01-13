8eea8a9f61a57d0a5e1ca6f9910db3324b183a7c
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-01-13 14:33:50 +0900

    Feat: 최대 매출 (SlidingWindow)
    Feat: 최대 매출 (SlidingWindow)

--- src/two_pointers/A_SlidingWindow3.java
+++ src/two_pointers/A_SlidingWindow3.java
@@ -39,6 +39,37 @@ import java.util.Scanner;
  */
 public class A_SlidingWindow3 {
 
+    /**
+     * 재 복습: 2025-01-13
+     */
+    public static class ASlidingWindow8 {
+        public static void main(String[] args) {
+            ASlidingWindow8 aSlidingWindow8 = new ASlidingWindow8();
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer order = scan.nextInt();
+            Integer[] intArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                intArray[i] = scan.nextInt();
+            }
+            aSlidingWindow8.solution(length, order, intArray);
+        }
+        public void solution(Integer length, Integer order, Integer[] intArray) {
+            int currentSum = 0;
+            int max = 0;
+            for(int i = 0; i < order; i++) {
+                currentSum = currentSum + intArray[i];
+            }
+            max = currentSum;
+
+            for(int right = order; right < length; right++) {
+                currentSum = currentSum + (intArray[right] - intArray[right - order]);
+                max = Math.max(max, currentSum);
+            }
+            System.out.println(max);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Integer length = scan.nextInt();
