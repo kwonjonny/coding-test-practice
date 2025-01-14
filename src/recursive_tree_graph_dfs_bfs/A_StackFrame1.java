8628c0694e75025eef5f235c265582bca234046e
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-01-14 15:39:57 +0900

    Feat: 재귀 함수 (DFS)
    Feat: 재귀 함수 (DFS)

--- src/recursive_tree_graph_dfs_bfs/A_StackFrame1.java
+++ src/recursive_tree_graph_dfs_bfs/A_StackFrame1.java
@@ -20,6 +20,25 @@ import java.util.Scanner;
  * 1 2 3
  */
 public class A_StackFrame1 {
+
+    /**
+     * 재 복습: 2025-01-14
+     */
+    public static class AStackFrame1 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            Integer order = scan.nextInt();
+            DFS(order);
+        }
+        public static void DFS(int order) {
+            if(order == 0) return;
+            else {
+                DFS(order - 1);
+                System.out.print(order + " ");
+            }
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Integer order = scan.nextInt();
