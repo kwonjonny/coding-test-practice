639d8b23369c87b7cd7c635196713da8d873b769
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-01-14 15:59:25 +0900

    Feat: 중복 확인 (HashSet OR Array로 찾기)
    Feat: 중복 확인 (HashSet OR Array로 찾기)

--- src/sorting_and_searching_solve/A_Searching1.java
+++ src/sorting_and_searching_solve/A_Searching1.java
@@ -37,6 +37,30 @@ import java.util.Scanner;
 public class A_Searching1 {
 
     /**
+     * 재 복습: 2025-01-14
+     */
+    public static class ASearching2 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer[] intArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                intArray[i] = scan.nextInt();
+            }
+            ASearching2.solution(length, intArray);
+        }
+        public static void solution(Integer length, Integer[] intArray) {
+            Arrays.sort(intArray);
+            String answer = "";
+            for(int i = 0; i < length -1; i++) {
+                if(intArray[i].equals(intArray[i + 1])) answer = "D";
+                else answer = "U";
+            }
+            System.out.println(answer);
+        }
+    }
+
+    /**
      * 재 복습: 2025-01-11
      */
     public static class ASearching1 {
