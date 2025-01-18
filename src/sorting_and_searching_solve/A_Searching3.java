1fdaa377a7cde72fede92217250643746cb6bf77
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-01-18 19:08:18 +0900

    Feat: 이분 검색
    Feat: 이분 검색

--- src/sorting_and_searching_solve/A_Searching3.java
+++ src/sorting_and_searching_solve/A_Searching3.java
@@ -30,6 +30,38 @@ import java.util.Scanner;
 public class A_Searching3 {
 
     /**
+     * 재 복습: 2025-01-18 
+     */
+    public static class ASearching6 {
+        public static void main(String[] args) {
+            Scanner scan= new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer target = scan.nextInt();
+            Integer[] intArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                intArray[i] = scan.nextInt();
+            }
+            ASearching6.solution(length, target, intArray);
+        }
+        public static void solution(Integer length, Integer target, Integer[] intArray) {
+            Arrays.sort(intArray);
+            int left = 0; int right = length - 1;
+            int answer = 0;
+            while(right >= left) {
+                int mid = (left + right) / 2;
+                if(intArray[mid].equals(target)) {
+                    answer = mid + 1;
+                    break;
+                }
+                else if(intArray[mid] > target) mid = right--;
+                else mid = left++;
+            }
+            System.out.println(answer);
+        }
+
+    }
+
+    /**
      * 재 복습: 2025-01-14
      */
     public static class ASearching5 {
