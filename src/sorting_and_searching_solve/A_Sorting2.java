e53b320d84a33a1cbe62d7758ea942996f5925fa
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-01-19 14:52:37 +0900

    Feat: 버블 정렬
    Feat: 버블 정렬

--- src/sorting_and_searching_solve/A_Sorting2.java
+++ src/sorting_and_searching_solve/A_Sorting2.java
@@ -31,6 +31,35 @@ import java.util.Scanner;
 public class A_Sorting2 {
 
     /**
+     * 재 복습: 2025-01-19
+     */
+    public static class ASorting4 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer[] intArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                intArray[i] = scan.nextInt();
+            }
+            ASorting4.solution(length, intArray);
+        }
+        public static void solution(Integer length, Integer[] intArray) {
+            for(int i = 0; i < length - 1; i++) {
+                for(int j = 0; j < length - 1 - i; j++) {
+                    if(intArray[j] > intArray[j + 1]) {
+                        int temp = intArray[j + 1];
+                        intArray[j + 1] = intArray[j];
+                        intArray[j] = temp;
+                    }
+                }
+            }
+            for(Integer x : intArray) {
+                System.out.print(x + " ");
+            }
+        }
+    }
+
+    /**
      * 재 복습: 2025-01-18
      */
     public static class ASorting3 {
