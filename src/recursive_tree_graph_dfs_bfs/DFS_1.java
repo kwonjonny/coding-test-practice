847c38339dcdb25e47b896064eb669fe3b715d6a
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-01-24 13:05:17 +0900

    Feat: 이진트리 순회 (DFS)
    Feat: 이진트리 순회 (DFS)

--- src/recursive_tree_graph_dfs_bfs/DFS_1.java
+++ src/recursive_tree_graph_dfs_bfs/DFS_1.java
@@ -21,72 +21,34 @@ import java.util.Scanner;
  */
 public class DFS_1 {
 
-    /**
-     * 재 복습: 2025-01-19
-     */
-    public static class DFS1 {
-        public static class Node {
-            int data;
-            Node lt, rt;
-            public Node(int val) {
-                this.data = val;
-                this.rt = null;
-                this.lt = null;
-            }
-        }
-        Node root;
-        public static void main(String[] args) {
-            DFS1 tree = new DFS1();
-            tree.root = new Node(1);
-            tree.root.lt = new Node(2);
-            tree.root.lt.lt = new Node(4);
-            tree.root.lt.rt = new Node(5);
-            tree.root.rt = new Node(3);
-            tree.root.rt.lt = new Node(6);
-            tree.root.rt.rt = new Node(7);
-            DFS1.DFS(tree.root);
-        }
-        public static void DFS(Node root) {
-            if(root == null) return;
-            else {
-                System.out.println(root.data);
-                DFS(root.lt);
-                DFS(root.rt);
-            }
-        }
-    }
-
     public static class Node {
+        Node right, left;
         int data;
-        Node lt, rt;
-        public Node(int val) {
-            this.data = val;
-            this.lt = null;
-            this.rt = null;
+        public Node(int V) {
+            this.data = V;
+            this.right = null;
+            this.left = null;
         }
     }
-
     Node root;
-    public static void DFS(Node root) {
-        if(root == null) return;
-        else {
-//            System.out.print(root.data + " "); 전위
-            DFS(root.lt);
-//            System.out.print(root.data + " "); 중위
-            DFS(root.rt);
-//            System.out.print(root.data + " "); 후위 
-        }
-    }
 
     public static void main(String[] args) {
         DFS_1 tree = new DFS_1();
         tree.root = new Node(1);
-        tree.root.lt = new Node(2);
-        tree.root.rt = new Node(3);
-        tree.root.lt.lt = new Node(4);
-        tree.root.lt.rt = new Node(5);
-        tree.root.rt.lt = new Node(6);
-        tree.root.rt.rt = new Node(7);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
         DFS(tree.root);
+    }
+    public static void DFS(Node node) {
+        if(node == null) return;
+        else {
+            System.out.print(node.data + " ");
+            DFS(node.left);
+            DFS(node.right);
+        }
     }
 }
