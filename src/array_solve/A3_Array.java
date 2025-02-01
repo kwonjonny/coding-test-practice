db3296223057ecfdf68190e7b2c0610282b4d486
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-01 17:23:35 +0900

    Feat: 가위 바위 보 (Array)
    Feat: 가위 바위 보 (Array)

--- src/array_solve/A3_Array.java
+++ src/array_solve/A3_Array.java
@@ -45,6 +45,38 @@ import java.util.Scanner;
 public class A3_Array {
 
     /**
+     * 재 복습: 2025-02-01
+     */
+    public static class A3Array10 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer[] aArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                aArray[i] = scan.nextInt();
+            }
+            Integer[] bArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                bArray[i] = scan.nextInt();
+            }
+            A3Array10.solution(length, aArray, bArray);
+        }
+        public static void solution(Integer length, Integer[] aArray, Integer[] bArray) {
+            List<String> answer = new ArrayList<>();
+            for(int i = 0; i < length; i++) {
+                Integer a = aArray[i];
+                Integer b = bArray[i];
+                if(a.equals(b)) answer.add("D");
+                else if(a.equals(1) && b.equals(3) || a.equals(2) && b.equals(1) || a.equals(3) && b.equals(2)) answer.add("A");
+                else answer.add("B");
+            }
+            for(String x : answer) {
+                System.out.println(x);
+            }
+        }
+    }
+
+    /**
      * 재 복습: 2025-01-14
      */
     public static class A3Array9 {
