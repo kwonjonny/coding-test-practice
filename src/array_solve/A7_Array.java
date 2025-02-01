c94970785cca32c1e297914134c7d19a87667802
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-01 17:42:28 +0900

    Feat: 점수 계산 (Array)
    Feat: 점수 계산 (Array)

--- src/array_solve/A7_Array.java
+++ src/array_solve/A7_Array.java
@@ -42,6 +42,33 @@ import java.util.Scanner;
 public class A7_Array {
 
     /**
+     * 재 복습: 2025-02-01
+     */
+    public static class A7Array11 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            Integer length = scan.nextInt();
+            Integer[] intArray = new Integer[length];
+            for(int i = 0; i < length; i++) {
+                intArray[i] = scan.nextInt();
+            }
+            A7Array11.solution(length, intArray);
+        }
+        public static void solution(Integer length, Integer[] intArray) {
+            int cnt = 0;
+            int answer = 0;
+            for(int i = 0; i < length; i++) {
+                if(intArray[i] == 0) cnt = 0;
+                else {
+                    cnt++;
+                }
+                answer = answer + cnt;
+            }
+            System.out.println(answer);
+        }
+    }
+
+    /**
      * 재 복습: 2025-01-14
      */
     public static class A7Array10 {
