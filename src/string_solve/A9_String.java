a73cc9b8f6706770737ca65aa014509cbb7158eb
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-01 16:51:58 +0900

    Feat: 숫자만 추출 (String)
    Feat: 숫자만 추출 (String)

--- src/string_solve/A9_String.java
+++ src/string_solve/A9_String.java
@@ -29,6 +29,24 @@ import java.util.Scanner;
 public class A9_String {
 
     /**
+     * 재 복습: 2025-02-01
+     */
+    public static class A9String8 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String str = scan.next();
+            A9String8.solution(str);
+        }
+        public static void solution(String str) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for(Character x : str.toCharArray()) {
+                if(Character.isDigit(x)) stringBuilder.append(x);
+            }
+            System.out.println(Integer.parseInt(stringBuilder.toString()));
+        }
+    }
+
+    /**
      * 재 복습: 2024-01-14
      */
     public static class A9String7 {
