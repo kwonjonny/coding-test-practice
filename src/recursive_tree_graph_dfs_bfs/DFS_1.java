965f31a223becbc719f10fdd523d424e3a68eb38
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-04 15:52:27 +0900

    Feat: 이진트리 순회 (DFS)
    Feat: 이진트리 순회 (DFS)

--- src/recursive_tree_graph_dfs_bfs/DFS_1.java
+++ src/recursive_tree_graph_dfs_bfs/DFS_1.java
@@ -1,6 +1,5 @@
 package recursive_tree_graph_dfs_bfs;
 
-import java.util.Scanner;
 
 /**
  * 이진트리 순회 (깊이우선탐색)
@@ -20,6 +19,42 @@ import java.util.Scanner;
  *           후위 4 5 2 6 7 3 1
  */
 public class DFS_1 {
+
+    /**
+     * 재 복습: 2025-02-04
+     */
+    public static class DFS1 {
+        public static class Node {
+            Node lt, rt;
+            int data;
+            public Node(int data) {
+                this.data = data;
+                this.lt = null;
+                this.rt = null;
+            }
+        }
+        Node root;
+
+        public static void main(String[] args) {
+            DFS1 tree = new DFS1();
+            tree.root = new Node(1);
+            tree.root.lt = new Node(2);
+            tree.root.rt = new Node(3);
+            tree.root.lt.lt = new Node(4);
+            tree.root.lt.rt = new Node(5);
+            tree.root.rt.lt = new Node(6);
+            tree.root.rt.rt = new Node(7);
+            DFS(tree.root);
+        }
+        public static void DFS(Node node) {
+            if(node == null) return;
+            else {
+                System.out.print(node.data + " ");
+                DFS(node.lt);
+                DFS(node.rt);
+            }
+        }
+    }
 
     public static class Node {
         Node right, left;
