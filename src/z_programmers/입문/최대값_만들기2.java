4da1e185ef4d8c58ebb3881a277ef89c46ab471e
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-11 10:45:14 +0900

    Feat: 최대값 만들기 2
    Feat: 최대값 만들기 2

---
+++ src/z_programmers/입문/최대값_만들기2.java
@@ -0,0 +1,40 @@
+package z_programmers.입문;
+
+import java.util.*;
+
+/**
+ * 문제 설명
+ * 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항
+ * -10,000 ≤ numbers의 원소 ≤ 10,000
+ * 2 ≤ numbers 의 길이 ≤ 100
+ * 입출력 예
+ * numbers	                    result
+ * [1, 2, -3, 4, -5]	        15
+ * [0, -31, 24, 10, 1, 9]	    240
+ * [10, 20, 30, 5, 5, 20, 5]	600
+ *
+ * 입출력 예 설명
+ * 입출력 예 #1
+ * 두 수의 곱중 최댓값은 -3 * -5 = 15 입니다.
+ *
+ * 입출력 예 #2
+ * 두 수의 곱중 최댓값은 10 * 24 = 240 입니다.
+ *
+ * 입출력 예 #3
+ * 두 수의 곱중 최댓값은 20 * 30 = 600 입니다.
+ */
+public class 최대값_만들기2 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, -3, 4, -5};
+        int answer = solution(numbers);
+        System.out.println(answer);
+    }
+    public static int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        answer = Math.max(numbers[numbers.length - 1] * numbers[numbers.length - 2], numbers[0] * numbers[1]);
+        return answer;
+    }
+}
