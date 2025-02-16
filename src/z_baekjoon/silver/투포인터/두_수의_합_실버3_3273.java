226fe247dbeafb1be46aa712b4decfd485db82f5
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-16 23:20:45 +0900

    Feat: 두 수의 합 3273 (실버3)
    Feat: 두 수의 합 3273 (실버3)

---
+++ src/z_baekjoon/silver/투포인터/두_수의_합_실버3_3273.java
@@ -0,0 +1,35 @@
+package z_baekjoon.silver.투포인터;
+
+import java.util.*;
+public class 두_수의_합_실버3_3273 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[] array = new int[N];
+        for(int i = 0; i < N; i++) {
+            array[i] = scan.nextInt();
+        }
+        int target = scan.nextInt();
+        int answer = solution(N, array, target);
+        System.out.println(answer);
+    }
+    public static int solution(int N, int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        int answer = 0;
+        Arrays.sort(array);
+        while(left < right) {
+            int currentSum = array[left] + array[right];
+            if(currentSum == target) {
+                answer++;
+                left++;
+                right--;
+            }
+            else if (target > currentSum) {
+                left++;
+            }
+            else right--;
+        }
+       return answer;
+    }
+}
