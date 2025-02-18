bf6cd0acd8ea40d35574f9e64a4ba20d3e29bdf9
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-18 16:05:03 +0900

    Feat: 백설 공주와 일곱 난쟁이 3040 (브론즈2)
    Feat: 백설 공주와 일곱 난쟁이 3040 (브론즈2)

---
+++ src/z_baekjoon/구현/백설_공주와_일곱_난쟁이_브론즈2_3040.java
@@ -0,0 +1,33 @@
+package z_baekjoon.구현;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 백설_공주와_일곱_난쟁이_브론즈2_3040 {
+    public static void main(String[] args) {
+        int N = 9;
+        int sum = 0;
+        int[] array = new int[N];
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0; i < N; i++) {
+            array[i] = scan.nextInt();
+            sum += array[i];
+        }
+
+        Arrays.sort(array);
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(sum - array[i] - array[j] == 100) {
+                    for(int k = 0; k < N; k++) {
+                        if(k == i || k == j) {
+                            continue;
+                        }
+                        System.out.println(array[k]);
+                    }
+                    System.exit(0);
+                }
+            }
+        }
+    }
+}
