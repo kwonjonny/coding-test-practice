4c1e1f0ac764ebb349b29b9375d54e513b20d2d5
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-18 15:34:04 +0900

    Feat: 구구단 2739 (브론즈5)
    Feat: 구구단 2739 (브론즈5)

---
+++ src/z_baekjoon/구현/구구단_브론즈5_2739.java
@@ -0,0 +1,16 @@
+package z_baekjoon.구현;
+
+import java.util.*;
+
+public class 구구단_브론즈5_2739 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        solution(N);
+    }
+    public static void solution(int N) {
+        for(int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + (N * i));
+        }
+    }
+}
