f8eda683c9266a823e83cefb055f6ab4e62a2362
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-18 10:47:16 +0900

    Feat: 두 용액 2470 (골드5)
    Feat: 두 용액 2470 (골드5)

---
+++ src/z_baekjoon/silver/투포인터/두_용액_골드5_2470.java
@@ -0,0 +1,40 @@
+package z_baekjoon.silver.투포인터;
+
+import java.util.*;
+
+public class 두_용액_골드5_2470 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[] array = new int[N];
+        for(int i = 0; i < N; i++) {
+            array[i] = scan.nextInt();
+        }
+        solution(N, array);
+    }
+    public static void solution(int N, int[] array) {
+        Arrays.sort(array);
+
+        int left = 0;
+        int right = array.length - 1;
+        int[] answer = new int[2];
+        int closestSum = Integer.MAX_VALUE;
+
+        while (right > left) {
+            int currentSum = array[left] + array[right];
+            if(Math.abs(currentSum) < Math.abs(closestSum)) {
+                closestSum = currentSum;
+                answer[0] = array[left];
+                answer[1] = array[right];
+            }
+            if(currentSum == 0) {
+                break;
+            }
+            if(currentSum > 0) right--;
+            else left++;
+        }
+        for(int x : answer) {
+            System.out.println(x);
+        }
+    }
+}
