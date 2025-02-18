012bc4fb7c853221402f8c07a4301d75896bcfeb
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-18 14:25:55 +0900

    Feat: 주몽 1940 (실버4)
    Feat: 주몽 1940 (실버4)

---
+++ src/z_baekjoon/투포인터/주몽.java
@@ -0,0 +1,35 @@
+package z_baekjoon.투포인터;
+
+import java.util.*;
+
+public class 주몽 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int T = scan.nextInt();
+        int[] array = new int[N];
+        for(int i = 0; i < N; i++) {
+            array[i] = scan.nextInt();
+        }
+        solution(N, T, array);
+    }
+    public static void solution(int N, int T, int[] array) {
+        Arrays.sort(array);
+        int left = 0;
+        int right = array.length - 1;
+        int answer = 0;
+        while(right > left) {
+            int currentSum = array[left] + array[right];
+            if(currentSum == T) {
+                answer ++;
+                left++;
+                right--;
+            }
+            else if (currentSum > T) {
+                right--;
+            }
+            else left++;
+        }
+        System.out.println(answer);
+    }
+}
