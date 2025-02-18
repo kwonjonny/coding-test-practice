fd03acb1baa5e6076a9dd640d7184aef2717a912
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-18 16:38:23 +0900

    Feat: N 찍기 2741 (브론즈5)
    Feat: N 찍기 2741 (브론즈5)

---
+++ src/z_baekjoon/구현/N_찍기_브론즈5_2741.java
@@ -0,0 +1,21 @@
+package z_baekjoon.구현;
+
+import java.util.*;
+
+public class N_찍기_브론즈5_2741 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        solution(N);
+    }
+    public static void solution(int N) {
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 1; i<= N; i++) {
+            answer.add(i);
+        }
+        Collections.sort(answer);
+        for(int x : answer) {
+            System.out.println(x);
+        }
+    }
+}
