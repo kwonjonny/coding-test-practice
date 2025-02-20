404aacce09c39162c95329a7d00cecc1e586186a
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-20 13:53:42 +0900

    Feat: 꼬마 정민 11382 (브론즈5)
    Feat: 꼬마 정민 11382 (브론즈5)

---
+++ src/z_baekjoon/구현/브론즈/꼬마_정민_브론즈5_11382.java
@@ -0,0 +1,14 @@
+package z_baekjoon.구현.브론즈;
+
+import java.util.Scanner;
+
+public class 꼬마_정민_브론즈5_11382 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long A = scan.nextLong();
+        long B = scan.nextLong();
+        long C = scan.nextLong();
+
+        System.out.println(A + B + C); 
+    }
+}
