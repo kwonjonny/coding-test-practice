f85ed6d6ea2b2c568ae5d9669e15b87a36cfbe32
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-23 17:48:03 +0900

    Feat: 큰돌의 터전 C1 (백준 2979 트럭 주차) 브론즈2
    Feat: 큰돌의 터전 C1 (백준 2979 트럭 주차) 브론즈2

---
+++ src/알고리즘/큰돌의터전/일주차/C1.java
@@ -0,0 +1,31 @@
+package 알고리즘.큰돌의터전.일주차;
+
+import java.util.Scanner;
+
+public class C1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+
+        int[] array = new int[101];
+        for(int i = 0; i < 3; i++) {
+            int start = scan.nextInt();
+            int end = scan.nextInt();
+            for(int j = start; j < end; j++) {
+                array[j]++;
+            }
+        }
+
+        int answer = 0;
+        for(int i = 1; i < 100; i++) {
+            if(array[i] != 0) {
+                if(array[i] == 1) answer += A * array[i];
+                else if(array[i] == 2) answer += B * array[i];
+                else if(array[i] == 3) answer += C * array[i];
+            }
+        }
+        System.out.println(answer);
+    }
+}
