10e19cd9ec212234f350fc356b49dfcc1b0c9e56
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-25 08:53:42 +0900

    Feat: 큰돌의 터전 브론즈4 (10808)
    Feat: 큰돌의 터전 브론즈4 (10808)

--- src/알고리즘/큰돌의터전/일주차/B1.java
+++ src/알고리즘/큰돌의터전/일주차/B1.java
@@ -17,4 +17,22 @@ public class B1 {
             System.out.print(x + " ");
         }
     }
+
+    /**
+     * 재 복습: 2025-02-25
+     */
+    public static class B12 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String str = scan.next();
+            int[] alphabets = new int[26];
+            for(int i = 0; i < str.length(); i++) {
+                int findCharInt = str.charAt(i) - 'a';
+                alphabets[findCharInt]++;
+            }
+            for(int x : alphabets) {
+                System.out.print(x + " ");
+            }
+        }
+    }
 }
