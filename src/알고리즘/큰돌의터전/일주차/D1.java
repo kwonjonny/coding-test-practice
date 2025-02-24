a702f9123de1634a9a94c4116cff25ba7f9b4852
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-25 08:57:55 +0900

    Feat: 큰돌의 터전 브론즈3 (10988)
    Feat: 큰돌의 터전 브론즈3 (10988)

--- src/알고리즘/큰돌의터전/일주차/D1.java
+++ src/알고리즘/큰돌의터전/일주차/D1.java
@@ -12,4 +12,21 @@ public class D1 {
     public static int solution(String str) {
         return str.equals(new StringBuilder(str).reverse().toString()) ? 1 : 0;
     }
+
+    /**
+     * 재 복습: 2025-02-25
+     */
+    public static class D12 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String str = scan.next();
+            solution(str);
+        }
+        public static void solution(String str) {
+            int answer = 0;
+            String reverse = new StringBuilder(str).reverse().toString();
+            if(reverse.equals(str)) answer = 1;
+            System.out.println(answer);
+        }
+    }
 }
