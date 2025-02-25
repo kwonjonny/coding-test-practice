892a9ccf9bafaf1e40afafeb4702fb39c55970cb
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-25 11:14:32 +0900

    Feat: 큰돌의터전 브론즈2 (1159)
    Feat: 큰돌의터전 브론즈2 (1159)

--- src/알고리즘/큰돌의터전/일주차/E1.java
+++ src/알고리즘/큰돌의터전/일주차/E1.java
@@ -25,4 +25,32 @@ public class E1 {
 
         System.out.println(stringBuilder);
     }
+
+    /**
+     * 재 복습: 2025-02-25
+     */
+    public static class E12 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int N = scan.nextInt();
+
+            int[] alphabets = new int[26];
+            for(int i = 0; i < N; i++) {
+                String str = scan.next();
+                for(int j = 0; j < 1; j++) {
+                    char findChar = str.charAt(j);
+                    alphabets[findChar - 'a']++;
+                }
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0; i < 26; i++) {
+                if(alphabets[i] >= 5) {
+                    stringBuilder.append((char) ('a' + i));
+                }
+            }
+            if(stringBuilder.length() == 0) stringBuilder.append("PREDAJA");
+            System.out.println(stringBuilder.toString());
+        }
+    }
 }
