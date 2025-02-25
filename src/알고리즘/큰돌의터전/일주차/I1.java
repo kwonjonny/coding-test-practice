ec41aef31740035675cbbd78db918b9eb4371a23
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-25 11:41:35 +0900

    Feat: 큰돌의 터전 실버4 (1620)
    Feat: 큰돌의 터전 실버4 (1620)

--- src/알고리즘/큰돌의터전/일주차/I1.java
+++ src/알고리즘/큰돌의터전/일주차/I1.java
@@ -30,4 +30,36 @@ public class I1 {
         }
         System.out.println(stringBuilder.toString());
     }
+
+    /**
+     * 재 복습: 2025-02-25
+     */
+    public static class I12 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int L = scan.nextInt();
+            int T = scan.nextInt();
+            Map<String, Integer> stringMap = new HashMap<>();
+            Map<Integer, String> integerMap = new HashMap<>();
+            for(int i = 1; i <= L; i++) {
+                String str = scan.next();
+                stringMap.put(str, i);
+                integerMap.put(i, str);
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0; i < T; i++) {
+                String str = scan.next();
+                if(Character.isDigit(str.charAt(0))) {
+                    String x = integerMap.get(Integer.parseInt(str));
+                    stringBuilder.append(x).append("\n");
+                }
+                else {
+                    int x = stringMap.get(str);
+                    stringBuilder.append(x).append("\n");
+                }
+            }
+            System.out.println(stringBuilder.toString());
+        }
+    }
 }
