7028196ff6ea64cdfa58270087d28e50d806fa8d
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-25 11:25:57 +0900

    Feat: 큰돌의 터전 실버3 (9996)
    Feat: 큰돌의 터전 실버3 (9996)

--- src/알고리즘/큰돌의터전/일주차/G1.java
+++ src/알고리즘/큰돌의터전/일주차/G1.java
@@ -7,7 +7,7 @@ public class G1 {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
         String[] array = new String[N + 1];
-        for(int i = 0; i < N +1; i++) {
+        for (int i = 0; i < N + 1; i++) {
             String x = scan.next();
             array[i] = x;
         }
@@ -16,16 +16,44 @@ public class G1 {
         String prefix = suffixPrefix[0];
         String suffix = suffixPrefix[1];
         StringBuilder stringBuilder = new StringBuilder();
-        for(int i = 1; i < array.length; i++) {
+        for (int i = 1; i < array.length; i++) {
             String x = array[i];
-            if(x.length() < suffix.length() + prefix.length()) {
+            if (x.length() < suffix.length() + prefix.length()) {
                 stringBuilder.append("NE").append("\n");
-            }
-            else if(x.startsWith(prefix) && x.endsWith(suffix)) {
+            } else if (x.startsWith(prefix) && x.endsWith(suffix)) {
                 stringBuilder.append("DA").append("\n");
-            }
-            else stringBuilder.append("NE").append("\n");
+            } else stringBuilder.append("NE").append("\n");
         }
         System.out.println(stringBuilder.toString());
+    }
+
+    /**
+     * 재 복습: 2025-02-25
+     */
+    public static class G12 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int N = scan.nextInt();
+            String[] array = new String[N + 1];
+            for (int i = 0; i < N + 1; i++) {
+                array[i] = scan.next();
+            }
+
+            String[] splitArray = array[0].split("\\*");
+            String prefix = splitArray[0];
+            String suffix = splitArray[1];
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 1; i < array.length; i++) {
+                String findString = array[i];
+                if (findString.startsWith(prefix) && findString.endsWith(suffix)) {
+                    stringBuilder.append("DA").append("\n");
+                } else if (findString.length() < prefix.length() + suffix.length()) {
+                    stringBuilder.append("NE").append("\n");
+                } else {
+                    stringBuilder.append("NE").append("\n");
+                }
+            }
+            System.out.println(stringBuilder.toString());
+        }
     }
 }
