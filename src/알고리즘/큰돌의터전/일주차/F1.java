315136a6863fc2db7946b3ca142642f0fdf1852f
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-25 11:14:02 +0900

    Feat: 큰돌의 터전 브론즈1 (11655)
    Feat: 큰돌의 터전 브론즈1 (11655)

--- src/알고리즘/큰돌의터전/일주차/F1.java
+++ src/알고리즘/큰돌의터전/일주차/F1.java
@@ -8,21 +8,46 @@ public class F1 {
         String str = scan.nextLine();
 
         StringBuilder stringBuilder = new StringBuilder();
-        for(int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             char findChar = str.charAt(i);
-            if(Character.isLowerCase(findChar)) {
-              int findCharInt = findChar - 'a';
-              char c = (char) ('a' + (findCharInt + 13) % 26);
-              stringBuilder.append(c);
-            }
-            else if (Character.isUpperCase(findChar)) {
+            if (Character.isLowerCase(findChar)) {
+                int findCharInt = findChar - 'a';
+                char c = (char) ('a' + (findCharInt + 13) % 26);
+                stringBuilder.append(c);
+            } else if (Character.isUpperCase(findChar)) {
                 int findCharInt = findChar - 'A';
                 char c = (char) ('A' + (findCharInt + 13) % 26);
                 stringBuilder.append(c);
-            }
-            else stringBuilder.append(findChar);
+            } else stringBuilder.append(findChar);
         }
 
         System.out.println(stringBuilder.toString());
+    }
+    
+    /**
+     * 재 복습: 2025-02-25
+     */
+    public static class F12 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String str = scan.nextLine();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < str.length(); i++) {
+                char findChar = str.charAt(i);
+                if (Character.isUpperCase(findChar)) {
+                    int parseIntChar = findChar - 'A';
+                    char parseChar = (char) ('A' + (parseIntChar + 13) % 26);
+                    stringBuilder.append(parseChar);
+                } else if (Character.isLowerCase(findChar)) {
+                    int parseIntChar = findChar - 'a';
+                    char parseChar = (char) ('a' + (parseIntChar + 13) % 26);
+                    stringBuilder.append(parseChar);
+                } else {
+                    stringBuilder.append(findChar);
+                }
+            }
+            System.out.println(stringBuilder.toString());
+        }
     }
 }
