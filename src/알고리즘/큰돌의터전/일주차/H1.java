3116207f18f032f6b116e06b61466b62256e2f50
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-26 22:59:33 +0900

    Feat: 큰돌의 터전 실버3 (2559)
    Feat: 큰돌의 터전 실버3 (2559)

--- src/알고리즘/큰돌의터전/일주차/H1.java
+++ src/알고리즘/큰돌의터전/일주차/H1.java
@@ -75,4 +75,32 @@ public class H1 {
             System.out.println(answer);
         }
     }
+
+
+    /**
+     * 재 복습: 2025-02-26
+     */
+    public static class H12_2 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int L = scan.nextInt();
+            int T = scan.nextInt();
+            int[] array = new int[L + 1];
+            for(int i = 1; i <= L; i++) {
+                array[i] = scan.nextInt();
+            }
+
+            int[] prefixSum = new int[L + 1];
+            for(int i = 1; i <= L; i++) {
+                prefixSum[i] = prefixSum[i - 1] + array[i];
+            }
+
+            int answer = 0;
+            for(int i = T; i < L; i++) {
+               answer = Math.max(answer,prefixSum[i] - prefixSum[i - T]);
+            }
+            System.out.println(answer);
+        }
+    }
+
 }
