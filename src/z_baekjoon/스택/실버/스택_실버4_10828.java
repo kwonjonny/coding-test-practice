e7b1e63e95cffe349a4f272d48fb8630b123c272
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-27 16:02:13 +0900

    Feat: 스택 실버4 (10828)
    Feat: 스택 실버4 (10828)

---
+++ src/z_baekjoon/스택/실버/스택_실버4_10828.java
@@ -0,0 +1,37 @@
+package z_baekjoon.스택.실버;
+
+import java.util.*;
+
+public class 스택_실버4_10828 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        scan.nextLine();
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder ret = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            String str = scan.nextLine();
+            if(str.startsWith("push")) {
+                String[] splitArray = str.split(" ");
+                int push = Integer.parseInt(splitArray[1]);
+                stack.push(push);
+            }
+            else if (str.equals("pop")) {
+                if(stack.isEmpty()) ret.append(-1).append("\n");
+                else ret.append(stack.pop()).append("\n");
+            }
+            else if (str.equals("size")) {
+                ret.append(stack.size()).append("\n");
+            }
+            else if (str.equals("empty")) {
+                if(stack.isEmpty()) ret.append(1).append("\n");
+                else ret.append(0).append("\n");
+            }
+            else if (str.equals("top")) {
+                if(stack.isEmpty()) ret.append(-1).append("\n");
+                else ret.append(stack.peek()).append("\n");
+            }
+        }
+        System.out.println(ret.toString());
+    }
+}
