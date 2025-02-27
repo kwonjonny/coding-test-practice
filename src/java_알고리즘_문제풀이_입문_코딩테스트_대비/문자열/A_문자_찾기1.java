d3b04e2bdd74949ee8ed21d38aeb3597aefef796
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-28 08:58:22 +0900

    Feat: 문자 찾기1
    Feat: 문자 찾기1

---
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/문자열/A_문자_찾기1.java
@@ -0,0 +1,51 @@
+package java_알고리즘_문제풀이_입문_코딩테스트_대비.문자열;
+
+/**
+ * 1. 문자 찾기
+ * 설명
+ * <p>
+ * 한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+ * <p>
+ * 대소문자를 구분하지 않습니다.문자열의 길이는 100을 넘지 않습니다.
+ * <p>
+ * <p>
+ * 입력
+ * <p>
+ * 첫 줄에 문자열이 주어지고, 두 번째 줄에 문자가 주어진다.
+ * <p>
+ * 문자열은 영어 알파벳으로만 구성되어 있습니다.
+ * <p>
+ * <p>
+ * 출력
+ * <p>
+ * 첫 줄에 해당 문자의 개수를 출력한다.
+ * <p>
+ * <p>
+ * 예시 입력 1
+ * <p>
+     Computercooler
+     c
+ * 예시 출력 1
+ * <p>
+ * 2
+ */
+import java.util.*;
+
+public class A_문자_찾기1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        char c = scan.next().charAt(0);
+        int answer = solution(str, c);
+        System.out.println(answer);
+    }
+    public static int solution(String str, char c) {
+        int cnt = 0;
+        c = Character.toLowerCase(c);
+        str = str.toLowerCase();
+        for(char x : str.toCharArray()) {
+            if(x == c) cnt++;
+        }
+        return cnt;
+    }
+}
