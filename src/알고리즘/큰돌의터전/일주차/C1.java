d9332b01c47383a091bdfa108c90cbdc5d3b3c23
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-02-27 09:22:38 +0900

    Feat: 큰돌의 터전 브론즈2 (2979)
    Feat: 큰돌의 터전 브론즈2 (2979)

--- src/알고리즘/큰돌의터전/일주차/C1.java
+++ src/알고리즘/큰돌의터전/일주차/C1.java
@@ -89,4 +89,33 @@ public class C1 {
             System.out.println(answer);
         }
     }
+
+    /**
+     * 재 복습: 2025-02-27
+     */
+    public static class C12_2 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+            int C = scan.nextInt();
+            int[] array = new int[101];
+            for(int i = 0; i < 3; i++) {
+                int start = scan.nextInt();
+                int end = scan.nextInt();
+                for(int j = start; j < end; j++) {
+                    array[j]++;
+                }
+            }
+            int answer = 0;
+            for(int i = 1; i < 100; i++) {
+                if(array[i] != 0) {
+                    if (array[i] == 1) answer += array[i] * A;
+                    else if (array[i] == 2) answer += array[i] * B;
+                    else if (array[i] == 3) answer += array[i] * C;
+                }
+            }
+            System.out.println(answer);
+        }
+    }
 }
