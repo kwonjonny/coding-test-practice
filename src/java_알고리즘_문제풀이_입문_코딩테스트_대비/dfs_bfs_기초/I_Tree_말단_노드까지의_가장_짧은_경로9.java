b6289f7946f98ba42ae7a5a90f6ccf5af033d542
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-03 19:00:54 +0900

    Feat: Tree말단 노드까지의 가장 짧은 경로9
    Feat: Tree말단 노드까지의 가장 짧은 경로9

---
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/I_Tree_말단_노드까지의_가장_짧은_경로9.java
@@ -0,0 +1,43 @@
+package java_알고리즘_문제풀이_입문_코딩테스트_대비.dfs_bfs_기초;
+
+/**
+ * 9. Tree 말단 노드까지의 가장 짧은 경로
+ *
+ * 아래 그림과 같은 이진트리에서 루트 노드 1에서 말단노드까지의 길이 중 가장 짧은 길이를 구하는 프로그램을 작성하세요.
+ * 각 경로의 길이는 루트노드에서 말단노드까지 가는데 이동하는 횟수를 각 간선(에지)의 개수를 길이로 하겠습니다.
+ *          1
+ *      |       |
+ *      2       3
+ *   |     |
+ *   4     5
+ *   가장 짧은 길이는 3번 노드까지의 길이인 1이다.
+ */
+
+import java.util.*;
+
+public class I_Tree_말단_노드까지의_가장_짧은_경로9 {
+    public static void main(String[] args) {
+        I_Tree_말단_노드까지의_가장_짧은_경로9 T = new I_Tree_말단_노드까지의_가장_짧은_경로9();
+        T.root = new Node(1);
+        T.root.lt = new Node(2);
+        T.root.rt = new Node(3);
+        T.root.lt.lt = new Node(4);
+        T.root.lt.rt = new Node(5);
+        int answer = DFS(0, T.root);
+        System.out.println(answer);
+    }
+    public static int DFS(int L, Node root) {
+        if(root.lt == null && root.rt == null) return L;
+        else return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+    }
+    Node root;
+    public static class Node {
+        int data;
+        Node lt, rt;
+        public Node(int data) {
+            this.data = data;
+            this.lt = null;
+            this.rt = null;
+        }
+    }
+}
