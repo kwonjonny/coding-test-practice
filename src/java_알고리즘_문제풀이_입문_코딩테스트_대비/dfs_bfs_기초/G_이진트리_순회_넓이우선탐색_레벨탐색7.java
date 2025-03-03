94a3c824a4d819c6c2e7a579eb016f85a4e6f8b6
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-03 18:18:42 +0900

    Feat: 이진트리 순회 넓이우선탐색 레벨탐색7
    Feat: 이진트리 순회 넓이우선탐색 레벨탐색7

---
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/G_이진트리_순회_넓이우선탐색_레벨탐색7.java
@@ -0,0 +1,55 @@
+package java_알고리즘_문제풀이_입문_코딩테스트_대비.dfs_bfs_기초;
+
+/**
+ * 7. 이진트리 순회 (넓이우선탐색 : 레벨탐색)
+ *
+ * 아래 그림과 같은 이진트리를 레벨탐색 연습하세요.
+ *              1
+ *           |   |
+ *           2     3
+ *         |  |   |  |
+ *         4  5   6  7
+ *
+ * 레벨 탐색 순회 출력 : 1 2 3 4 5 6 7
+ */
+
+import java.util.*;
+
+public class G_이진트리_순회_넓이우선탐색_레벨탐색7 {
+    public static void main(String[] args) {
+        G_이진트리_순회_넓이우선탐색_레벨탐색7 T = new G_이진트리_순회_넓이우선탐색_레벨탐색7();
+        T.root = new Node(1);
+        T.root.lt = new Node(2);
+        T.root.rt = new Node(3);
+        T.root.lt.lt = new Node(4);
+        T.root.lt.rt = new Node(5);
+        T.root.rt.lt = new Node(6);
+        T.root.rt.rt = new Node(7);
+        BFS(T.root);
+    }
+    public static void BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while(!Q.isEmpty()) {
+           int length = Q.size();
+           for(int i = 0; i < length; i++) {
+               Node current = Q.poll();
+               System.out.println("level" + L + ": " + current.data + " ");
+               if(current.lt != null) Q.offer(current.lt);
+               if(current.rt != null) Q.offer(current.rt);
+           }
+           L++;
+        }
+    }
+    Node root;
+    public static class Node {
+        int data;
+        Node lt, rt;
+        public Node(int data) {
+            this.data = data;
+            this.lt = null;
+            this.rt = null;
+        }
+    }
+}
