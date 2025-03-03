235399e251d15f4b9bef01eccbde9296f3f88c6b
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-03 17:29:20 +0900

    Feat: 이진트리 순회 (깊이우선탐색)5
    Feat: 이진트리 순회 (깊이우선탐색)5

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/E_이진트리_순회_깊이우선탐색5.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/E_이진트리_순회_깊이우선탐색5.java
@@ -3,6 +3,17 @@ package java_알고리즘_문제풀이_입문_코딩테스트_대비.dfs_bfs_기
 /**
  * 5. 이진트리 순회 (깊이우선탐색)
  *
+ * 아래 그림과 같은 이진트리 전위순회와 중위순회 후위순회를 연습해보세요.
+ *
+ *              1
+ *            |   |
+ *           2     3
+ *         |  |   |  |
+ *         4  5   6  7
+ *
+ * 전위순회 출력: 1 2 4 5 3 6 7
+ * 중위순회 출력: 4 2 5 1 6 3 7
+ * 후위순회 출력: 4 5 2 6 7 3 1
  */
 
 public class E_이진트리_순회_깊이우선탐색5 {
@@ -20,9 +31,11 @@ public class E_이진트리_순회_깊이우선탐색5 {
     public static void DFS(Node root) {
         if(root == null) return;
         else {
-            System.out.print(root.data + " ");
+            System.out.print(root.data + " "); // 전위
             DFS(root.lt);
+            // System.out.print(root.data + " "); // 중위
             DFS(root.rt);
+            // System.out.print(root.data + " "); // 후위
         }
     }
     Node tree;
