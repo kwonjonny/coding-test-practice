9c70d95ae497d7b596521df304d12dc1d1bfad84
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-04 13:51:02 +0900

    Feat: 경로 탐색 인접리스트12
    Feat: 경로 탐색 인접리스트12

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/L_경로_탐색_인접리스트12.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/L_경로_탐색_인접리스트12.java
@@ -3,7 +3,7 @@ package java_알고리즘_문제풀이_입문_코딩테스트_대비.dfs_bfs_기
 /**
  * 12. 경로 탐색 (인접리스트)
  * <p>
- * 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하ㅔㅅ요.
+ * 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요.
  * 아래 그래프에서 1번 정점에서 5번 정점으로 가는 가지수는
  * 1 <-> 2 -> 5
  * |  X  |
@@ -42,6 +42,49 @@ package java_알고리즘_문제풀이_입문_코딩테스트_대비.dfs_bfs_기
 import java.util.*;
 
 public class L_경로_탐색_인접리스트12 {
+
+    /**
+     * 재 복습: 2025-03-04
+     */
+    public static class L_경로_탐색_인접리스트12_1 {
+        static List<List<Integer>> graph;
+        static int answer;
+        static int[] check;
+        static int N;
+        static int T;
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            N = scan.nextInt();
+            T = scan.nextInt();
+            graph = new ArrayList<>();
+            check = new int[N + 1];
+            for(int i = 0; i <= N; i++) {
+                graph.add(new ArrayList<Integer>());
+            }
+            for(int i = 0; i < T; i++) {
+                int a = scan.nextInt();
+                int b = scan.nextInt();
+                graph.get(a).add(b);
+            }
+            check[1] = 1;
+            DFS(1);
+            System.out.println(answer);
+        }
+        public static void DFS(int L) {
+            if(L == N) answer++;
+            else {
+                for(int nextValue : graph.get(L)) {
+                    if(check[nextValue] == 0) {
+                        check[nextValue] = 1;
+                        DFS(nextValue);
+                        check[nextValue] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+
     static int N;
     static int T;
     static List<List<Integer>> graph;
