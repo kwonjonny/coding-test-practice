1f263cbb40f69a39ed3444152fddac82e62bc9b4
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-05 13:00:11 +0900

    Feat: 괄호문자제거2 (재복습)
    Feat: 괄호문자제거2 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/스택_큐/B_괄호문자제거2.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/스택_큐/B_괄호문자제거2.java
@@ -28,6 +28,35 @@ package java_알고리즘_문제풀이_입문_코딩테스트_대비.스택_큐;
 import java.util.*;
 
 public class B_괄호문자제거2 {
+
+    /**
+     * 재 복습: 2025-03-05
+     */
+    public static class B_괄호문자제거2_1 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String str = scan.next();
+            String answer = solution(str);
+            System.out.println(answer);
+        }
+        public static String solution(String str) {
+            Stack<Character> stack = new Stack<>();
+            for(int i = 0; i < str.length(); i++) {
+                char findChar = str.charAt(i);
+                if(findChar != ')') stack.push(findChar);
+                else {
+                    while(stack.pop() != '(');
+                }
+            }
+            StringBuilder ret = new StringBuilder();
+            for(char x : stack){
+                ret.append(x);
+            }
+            return ret.toString();
+        }
+    }
+
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String str = scan.next();
