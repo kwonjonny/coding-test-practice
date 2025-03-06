bfee7beeffa2a0a0635908471cc36d3ff2a38f79
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-06 10:43:34 +0900

    Feat: 바둑이 승차 DFS2 (재복습)
    Feat: 바둑이 승차 DFS2 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_활용/B_바둑이_승차_DFS2.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_활용/B_바둑이_승차_DFS2.java
@@ -39,6 +39,38 @@ package java_알고리즘_문제풀이_입문_코딩테스트_대비.dfs_bfs_활
 import java.util.*;
 
 public class B_바둑이_승차_DFS2 {
+
+    /**
+     * 재 복습: 2025-03-06
+     */
+    public static class B_바둑이_승차_DFS2_1 {
+        static int N;
+        static int weight;
+        static int answer = Integer.MIN_VALUE;
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            weight = scan.nextInt();
+            N = scan.nextInt();
+            int[] array = new int[N];
+            for(int i = 0; i < N; i++) {
+                array[i] = scan.nextInt();
+            }
+            DFS(0, 0, array);
+            System.out.println(answer);
+        }
+        public static void DFS(int level, int sum, int[] array) {
+            if(sum > weight) return;
+            if(N == level) {
+                answer = Math.max(answer, sum);
+            }
+            else {
+                DFS(level + 1, sum + array[level], array);
+                DFS(level + 1, sum, array);
+            }
+        }
+    }
+
+
     static int answer = Integer.MIN_VALUE;
     static int N;
     static int weight;
