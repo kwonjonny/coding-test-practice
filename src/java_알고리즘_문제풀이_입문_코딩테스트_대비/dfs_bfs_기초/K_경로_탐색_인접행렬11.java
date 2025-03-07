384133e4242f68e9811f13549d48a691255c1740
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-07 14:02:45 +0900

    feat: 경로 탐색 인접행렬11 (재복습)
    feat: 경로 탐색 인접행렬11 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/K_경로_탐색_인접행렬11.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/K_경로_탐색_인접행렬11.java
@@ -44,6 +44,46 @@ import java.util.*;
 public class K_경로_탐색_인접행렬11 {
 
     /**
+     * 재 복습: 2025-03-07
+     */
+    public static class K_경로_탐색_인접행렬11_2 {
+        static int[][] graph;
+        static int N;
+        static int T;
+        static int[] check;
+        static int answer = 0;
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            N = scan.nextInt();
+            T = scan.nextInt();
+            check = new int[N + 1];
+            graph = new int[N + 1][N + 1];
+            for(int i = 0; i < T; i++) {
+                int a = scan.nextInt();
+                int b = scan.nextInt();
+                graph[a][b] = 1;
+            }
+            check[1] = -1;
+            DFS(1);
+            System.out.println(answer);
+        }
+        public static void DFS(int level) {
+            if(level == N) {
+                answer++;
+            } else {
+                for(int i = 1; i <= N; i++) {
+                    if(graph[level][i] == 1 && check[i] == 0) {
+                        check[i] = 1;
+                        DFS(i);
+                        check[i] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
      * 재 복습: 2025-03-04
      */
     public static class K_경로_탐색_인접행렬11_1 {
