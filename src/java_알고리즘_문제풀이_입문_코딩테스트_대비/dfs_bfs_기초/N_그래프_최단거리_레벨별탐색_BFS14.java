54208f00c54ff216acc532efbdf6394b93bfbb10
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-08 17:15:00 +0900

    Feat: 그래프 최단거리 레벨탐색 BFS14 (재복습)
    Feat: 그래프 최단거리 레벨탐색 BFS14 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/N_그래프_최단거리_레벨별탐색_BFS14.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/N_그래프_최단거리_레벨별탐색_BFS14.java
@@ -39,6 +39,58 @@ import java.util.*;
 public class N_그래프_최단거리_레벨별탐색_BFS14 {
 
     /**
+     * 재 복습: 2025-03-08
+     */
+    public static class N_그래프_최단거리_레벨탐색_BFS14_2 {
+        static int N;
+        static int T;
+        static List<List<Integer>> graph;
+        static int[] check;
+        static int[] distance;
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            N = scan.nextInt();
+            T = scan.nextInt();
+            check = new int[N + 1];
+            graph = new ArrayList<>();
+            distance = new int[N + 1];
+            for(int i = 0; i <= N; i++) {
+                graph.add(new ArrayList<>());
+            }
+            for(int i = 0; i < T; i++) {
+                int a = scan.nextInt();
+                int b = scan.nextInt();
+                graph.get(a).add(b);
+            }
+            BFS(1);
+            for(int i = 2; i < N; i++) {
+                System.out.println(i + " : " + distance[i]);
+            }
+        }
+        public static void BFS(int L) {
+            Queue<Integer> Q = new LinkedList<>();
+            Q.offer(L);
+            int level = 0;
+            check[L] = -1;
+            while(!Q.isEmpty()) {
+                int length = Q.size();
+                for(int i = 0; i < length; i++) {
+                    int nx = Q.poll();
+                    for(int nv : graph.get(nx)) {
+                        if(check[nv] == 0) {
+                            check[nv] = 1;
+                            distance[nv] = level + 1;
+                            Q.offer(nv);
+                        }
+                    }
+                }
+                level++;
+            }
+        }
+    }
+
+
+    /**
      * 재 복습: 2025-03-07
      */
     public static class N_그래프_최단거리_레벨별탐색_BFS14_1 {
