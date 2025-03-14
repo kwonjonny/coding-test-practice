aaad0b0fe1d5bdca7c0e6849a194332cc17645f8
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-14 15:52:32 +0900

    Feat: 말단 노드까지의 가장 짧은 경로9 (재복습)
    Feat: 말단 노드까지의 가장 짧은 경로9 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/I_Tree_말단_노드까지의_가장_짧은_경로9.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/I_Tree_말단_노드까지의_가장_짧은_경로9.java
@@ -18,6 +18,41 @@ import java.util.*;
 public class I_Tree_말단_노드까지의_가장_짧은_경로9 {
 
     /**
+     * 재 복습: 2025-03-14
+     */
+    public static class I_Tree_말단_노드까지의_가장_짧은_경로9_4 {
+        public static void main(String[] args) {
+            I_Tree_말단_노드까지의_가장_짧은_경로9_4 T = new I_Tree_말단_노드까지의_가장_짧은_경로9_4();
+            T.root = new Node(1);
+            T.root.lt = new Node(2);
+            T.root.rt = new Node(3);
+            T.root.lt.lt = new Node(4);
+            T.root.lt.rt = new Node(5);
+            int answer = DFS(0, T.root);
+            System.out.println(answer);
+        }
+        public static int DFS(int level, Node root) {
+            if(root.lt == null && root.rt == null) {
+                return level;
+            }
+            else {
+                return Math.min(DFS(level + 1, root.lt), DFS(level + 1, root.rt));
+            }
+        }
+        Node root;
+        public static class Node {
+            int data;
+            Node lt, rt;
+            public Node(int data) {
+                this.data = data;
+                this.lt = null;
+                this.rt = null;
+            }
+        }
+    }
+
+
+    /**
      * 재 복습: 2025-03-07
      */
     public static class I_Tree_말단_노드까지의_가장_짧은_경로9_3 {
