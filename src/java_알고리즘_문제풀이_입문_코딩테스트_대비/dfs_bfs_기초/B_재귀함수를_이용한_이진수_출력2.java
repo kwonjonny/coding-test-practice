04693cf0983635fbc36ece4ccd907d34d2c8b559
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-14 16:41:37 +0900

    Feat: 재귀함수를 이용한 이진수 출력2 (재복습)
    Feat: 재귀함수를 이용한 이진수 출력2 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/B_재귀함수를_이용한_이진수_출력2.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/B_재귀함수를_이용한_이진수_출력2.java
@@ -22,6 +22,24 @@ import java.util.*;
 
 public class B_재귀함수를_이용한_이진수_출력2 {
 
+    /**
+     * 재 복습: 2025-03-014
+     */
+    public static class B_재귀함수를_이용한_이진수_출력2_3 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int N = scan.nextInt();
+            DFS(N);
+        }
+        public static void DFS(int N) {
+            if(N == 0) return;
+            else {
+                DFS(N / 2);
+                System.out.print(N % 2);
+            }
+        }
+    }
+
 
     /**
      * 재 복습: 2025-03-05
