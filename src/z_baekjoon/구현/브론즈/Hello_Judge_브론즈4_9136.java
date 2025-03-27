2e7e16553af00f747912a0383fcbe8a4e8a4ed5f
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-03-27 14:18:12 +0900

    Feat: Heollo Judge 브론즈4 (9136)
    Feat: Heollo Judge 브론즈4 (9136)

---
+++ src/z_baekjoon/구현/브론즈/Hello_Judge_브론즈4_9136.java
@@ -0,0 +1,13 @@
+package z_baekjoon.구현.브론즈;
+
+import java.util.*;
+
+public class Hello_Judge_브론즈4_9136 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for(int i = 0; i < N; i++) {
+            System.out.println("Hello World, Judge " + (i + 1) + "!");
+        }
+    }
+}
