dd2926f1dd1e2c975cb2251374bc0f3fc0984246
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-04-10 17:04:09 +0900

    Feat: 블랙잭 브론즈2 (2798)
    Feat: 블랙잭 브론즈2 (2798)

--- src/z_baekjoon/완전탐색/브론즈/블랙잭_브론즈2_2798.java
+++ src/z_baekjoon/완전탐색/브론즈/블랙잭_브론즈2_2798.java
@@ -26,4 +26,32 @@ public class 블랙잭_브론즈2_2798 {
         }
         System.out.println(max);
     }
+
+
+    public static class 블랙잭_브론즈2_2798_1 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int N = scan.nextInt();
+            int T = scan.nextInt();
+            int[] array = new int[N];
+            for(int i = 0; i < N; i++) {
+                array[i] = scan.nextInt();
+            }
+            solution(N, T, array);
+        }
+        public static void solution(int N, int T, int[] array) {
+            int answer = 0;
+            for(int i = 0; i < N; i++) {
+                for(int j = i + 1; j < N; j++) {
+                    for(int k = j + 1; k < N; k++) {
+                        int sum = array[i] + array[j] + array[k];
+                        if(sum <= T && answer <= sum) {
+                            answer = sum;
+                        }
+                    }
+                }
+            }
+            System.out.println(answer);
+        }
+    }
 }
