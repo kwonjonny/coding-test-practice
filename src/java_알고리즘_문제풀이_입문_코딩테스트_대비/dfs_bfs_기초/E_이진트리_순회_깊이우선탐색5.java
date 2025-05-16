476fe6992284173e35372bccf0d1e21a833fdc5d
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-05-16 13:56:05 +0900

    Feat: 이진트리 순회 깊이우선탐색5 (재복습)
    Feat: 이진트리 순회 깊이우선탐색5 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/E_이진트리_순회_깊이우선탐색5.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/E_이진트리_순회_깊이우선탐색5.java
@@ -19,6 +19,47 @@ package java_알고리즘_문제풀이_입문_코딩테스트_대비.dfs_bfs_기
 public class E_이진트리_순회_깊이우선탐색5 {
 
     /**
+     * 재 복습: 2025-05-16
+     */
+    public static class E_이진트리_순회_깊이우선탐색5_5 {
+        public static void main(String[] args) {
+            E_이진트리_순회_깊이우선탐색5_5 tree = new E_이진트리_순회_깊이우선탐색5_5();
+            tree.root = new Node(1);
+            tree.root.lt = new Node(2);
+            tree.root.rt = new Node(3);
+            tree.root.lt.lt = new Node(4);
+            tree.root.lt.rt = new Node(5);
+            tree.root.rt.lt = new Node(6);
+            tree.root.rt.rt = new Node(7);
+            DFS(tree.root);
+        }
+        public static void DFS(Node root) {
+            if(root == null) return;
+            else {
+                // 전위
+                //System.out.print(root.data + " ");
+                DFS(root.lt);
+                // 중위
+                System.out.print(root.data + " ");
+                DFS(root.rt);
+                // 후위
+                //System.out.print(root.data + " ");
+            }
+        }
+        Node root;
+        public static class Node {
+            int data;
+            Node lt, rt;
+            public Node(int data) {
+                this.data = data;
+                this.lt = null;
+                this.rt = null;
+            }
+        }
+    }
+
+
+    /**
      * 재 복습: 2025-03-14
      */
     public static class E_이진트리_순회_깊이우선탐색5_4 {
