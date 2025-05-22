e9d131065bccbd0ae5247dcfc25e2e5d49f5b2f2
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-05-22 13:17:00 +0900

    Feat: 말단 노드까지의 가장 짧은 경로10 (재복습)
    Feat: 말단 노드까지의 가장 짧은 경로10 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/J_Tree_말단_노드까지의_가장_짧은_경로10.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/J_Tree_말단_노드까지의_가장_짧은_경로10.java
@@ -18,6 +18,49 @@ import java.util.*;
 public class J_Tree_말단_노드까지의_가장_짧은_경로10 {
 
     /**
+     * 재 복습: 2025-05-22
+     */
+    public static class J_Tree_말단_노드까지의_가장_짧은_경로10_6 {
+        public static void main(String[] args) {
+            J_Tree_말단_노드까지의_가장_짧은_경로10_6 T = new J_Tree_말단_노드까지의_가장_짧은_경로10_6();
+            T.root = new Node(1);
+            T.root.lt = new Node(2);
+            T.root.rt = new Node(3);
+            T.root.lt.lt = new Node(4);
+            T.root.lt.rt = new Node(5);
+            int answer = BFS(T.root);
+            System.out.println(answer);
+        }
+        public static int BFS(Node root) {
+            Queue<Node> Q = new LinkedList<>();
+            Q.offer(root);
+            int level = 0;
+            while(!Q.isEmpty()) {
+                int length = Q.size();
+                for(int i = 0; i < length; i++) {
+                    Node NX = Q.poll();
+                    if(NX.lt == null && NX.rt == null) return level;
+                    if(NX.lt != null) Q.offer(root.lt);
+                    if(NX.rt != null) Q.offer(root.rt);
+                }
+                level++;
+            }
+            return -1;
+        }
+        Node root;
+        public static class Node {
+            int data;
+            Node lt, rt;
+            public Node(int data) {
+                this.data = data;
+                this.lt = null;
+                this.rt = null;
+            }
+        }
+    }
+
+
+    /**
      * 재 복습: 2025-05-19
      */
     public static class J_Tree_말단_노드까지의_가장_짧은_경로10_5 {
