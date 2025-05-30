f2a51af7931859662def1ff4979d462bcf0bf1cf
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-05-30 13:26:27 +0900

    feat: 경로 탐색 인접리스트 12 (재복습)
    feat: 경로 탐색 인접리스트 12 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/L_경로_탐색_인접리스트12.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/L_경로_탐색_인접리스트12.java
@@ -44,6 +44,48 @@ import java.util.*;
 public class L_경로_탐색_인접리스트12 {
 
     /**
+     * 재 복습: 2025-05-30
+     */
+    public static class L_경로_탐색_인접리스트12_9 {
+        static int N;
+        static int T;
+        static int[] check;
+        static List<List<Integer>> graph;
+        static int answer = 0;
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            N = scan.nextInt();
+            T = scan.nextInt();
+            graph = new ArrayList<>();
+            check = new int[N + 1];
+            check[1] = 1;
+            for(int i = 0; i <= N; i++) {
+                graph.add(new ArrayList<>());
+            }
+            for(int i = 0; i < T; i++) {
+                int a = scan.nextInt();
+                int b = scan.nextInt();
+                graph.get(a).add(b);
+            }
+            DFS(1);
+            System.out.println(answer);
+        }
+        public static void DFS(int level) {
+            if(level == N) answer++;
+            else {
+                for(int NX : graph.get(level)) {
+                    if(check[NX] == 0) {
+                        check[NX] = 1;
+                        DFS(NX);
+                        check[NX] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
      * 재 복습:2025-05-22
      */
     public static class L_경로_탐색_인접리스트12_8 {
