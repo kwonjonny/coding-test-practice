5d342f8274b1f4c8183115f99500ba604e876e5d
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-06-03 16:51:30 +0900

    Feat: 순열 구하기 DFS6 (재복습)
    Feat: 순열 구하기 DFS6 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_활용/F_순열_구하기_DFS6.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_활용/F_순열_구하기_DFS6.java
@@ -30,6 +30,44 @@ import java.util.*;
 public class F_순열_구하기_DFS6 {
 
     /**
+     * 재 복습: 2025-06-03
+     */
+    public static class F_순열_구하기_DFS6_2 {
+        static int N;
+        static int T;
+        static int[] check, array, pm;
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            N = scan.nextInt();
+            T = scan.nextInt();
+            array = new int[N];
+            for(int i = 0; i < N; i++) {
+                array[i] = scan.nextInt();
+            }
+            check = new int[N];
+            pm = new int[T];
+            DFS(0);
+        }
+        public static void DFS(int level) {
+            if(level == T) {
+                for(int x : pm) System.out.print(x + " ");
+                System.out.println();
+            }
+            else {
+                for(int i = 0; i < N; i++) {
+                    if(check[i] == 0) {
+                        check[i] = 1;
+                        pm[level] = array[i];
+                        DFS(level + 1);
+                        check[i] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
      * 재 복습: 2025-06-02
      */
     public static class F_순열_구하기_DFS6_1 {
