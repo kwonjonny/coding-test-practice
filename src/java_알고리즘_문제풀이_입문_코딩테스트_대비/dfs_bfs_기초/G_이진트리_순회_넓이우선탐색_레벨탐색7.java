87b664725312d18fd35eb280381ad24fbe7a3b33
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-06-07 16:15:49 +0900

    Feat: 이진트리 순회 넓이우선탐색 레벨탐색7 (재복습)
    Feat: 이진트리 순회 넓이우선탐색 레벨탐색7 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/G_이진트리_순회_넓이우선탐색_레벨탐색7.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/dfs_bfs_기초/G_이진트리_순회_넓이우선탐색_레벨탐색7.java
@@ -18,6 +18,47 @@ import java.util.*;
 public class G_이진트리_순회_넓이우선탐색_레벨탐색7 {
 
     /**
+     * 재 복습: 2025-06-07
+     */
+    public static class G_이진트리_순회_넓이우선탐색_레벨탐색7_9 {
+        public static class Node {
+            int data;
+            Node lt, rt;
+            public Node(int data) {
+                this.data = data;
+                this.lt = null;
+                this.rt = null;
+            }
+        }
+        Node root;
+        public static void main(String[] args) {
+            G_이진트리_순회_넓이우선탐색_레벨탐색7_9 T = new G_이진트리_순회_넓이우선탐색_레벨탐색7_9();
+            T.root = new Node(1);
+            T.root.lt = new Node(2);
+            T.root.rt = new Node(3);
+            T.root.lt.lt = new Node(4);
+            T.root.lt.rt = new Node(5);
+            T.root.rt.lt = new Node(6);
+            T.root.rt.rt = new Node(7);
+            BFS(T.root);
+        }
+        public static void BFS(Node root) {
+            Queue<Node> Q = new LinkedList<>();
+            Q.offer(root);
+            while (!Q.isEmpty()) {
+                int length = Q.size();
+                for(int i = 0; i < length; i++) {
+                    Node NX = Q.poll();
+                    System.out.print(NX.data + " ");
+                    if(NX.lt != null) Q.offer(NX.lt);
+                    if(NX.rt != null) Q.offer(NX.rt);
+                }
+            }
+        }
+    }
+
+
+    /**
      * 재 복습: 2025-05-30
      */
     public static class G_이진트리_순회_넓이우선탐색_레벨탐색7_8 {
