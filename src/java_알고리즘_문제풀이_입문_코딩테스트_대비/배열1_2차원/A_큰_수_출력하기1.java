85fe098573e1696da70a73d209e05ef11295cd74
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-07-29 08:44:45 +0900

    Feat: 큰 수 출력하기1 (재복습)
    Feat: 큰 수 출력하기1 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/배열1_2차원/A_큰_수_출력하기1.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/배열1_2차원/A_큰_수_출력하기1.java
@@ -33,76 +33,10 @@ import java.util.*;
 public class A_큰_수_출력하기1 {
 
     /**
-     * 재 복습: 2025-04-29
-     */
-    public static class A_큰_수_출력하기1_3 {
-        public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            int N = scan.nextInt();
-            int[] array = new int[N];
-            for(int i = 0; i < N; i++) {
-                array[i] = scan.nextInt();
-            }
-            solution(N, array);
-        }
-        public static void solution(int N, int[] array) {
-            List<Integer> answer = new ArrayList<>();
-            answer.add(array[0]);
-            for(int i = 1; i < N; i++) {
-                if(array[i] > array[i - 1]) answer.add(array[i]);
-            }
-            for(int x : answer) {
-                System.out.print(x + " ");
-            }
-        }
-    }
-
-
-    /**
-     * 재 복습: 2025-04-02
-     */
-    public static class A_큰_수_출력하기1_2 {
-        public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            int N = scan.nextInt();
-            int[] array = new int[N];
-            for(int i = 0; i < N; i++) {
-                array[i] = scan.nextInt();
-            }
-            solution(N, array);
-        }
-        public static void solution(int N, int[] array) {
-            List<Integer> answer = new ArrayList<>();
-            answer.add(array[0]);
-            for(int i = 1; i < N; i++) {
-                if(array[i] > array[i - 1]) {
-                   answer.add(array[i]);
-                }
-            }
-            for(int x : answer) {
-                System.out.print(x + " ");
-            }
-        }
-    }
-
-    
-    /**
-     * 재 복습: 2025-03-06
+     * 재 복습: 2025-07-29
      */
     public static class A_큰_수_출력하기1_1 {
-        public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            int N = scan.nextInt();
-            int[] array = new int[N];
-            for(int i = 0; i < N; i++) {
-                array[i] = scan.nextInt();
-            }
-            int[] answer = solution(N, array);
-            for(int x : answer) {
-                System.out.print(x + " ");
-            }
-        }
-        public static int[] solution(int N, int[] array) {
+        public static void solution(int N, int[] array) {
             List<Integer> answer = new ArrayList<>();
             answer.add(array[0]);
             for(int i = 1; i < N; i++) {
@@ -110,10 +44,20 @@ public class A_큰_수_출력하기1 {
                     answer.add(array[i]);
                 }
             }
-            return answer.stream().mapToInt(e -> e).toArray();
+            for(int x : answer) {
+                System.out.print(x + " ");
+            }
+        }
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int N = scan.nextInt();
+            int[] array = new int[N];
+            for(int i = 0; i < N; i++) {
+                array[i] = scan.nextInt();
+            }
+            solution(N, array);
         }
     }
-
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
