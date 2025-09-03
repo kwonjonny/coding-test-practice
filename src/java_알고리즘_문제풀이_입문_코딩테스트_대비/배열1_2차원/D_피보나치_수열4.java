0a42c47e4bca993afbec091d1f5f3a510b57a778
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-09-03 16:35:20 +0900

    Feat: 피보나치 수열4 (재복습)
    Feat: 피보나치 수열4 (재복습)

--- src/java_알고리즘_문제풀이_입문_코딩테스트_대비/배열1_2차원/D_피보나치_수열4.java
+++ src/java_알고리즘_문제풀이_입문_코딩테스트_대비/배열1_2차원/D_피보나치_수열4.java
@@ -31,49 +31,26 @@ import java.util.*;
 
 public class D_피보나치_수열4 {
 
-    /**
-     * 재 복습: 2025-07-29
-     */
     public static class D_피보나치_수열4_1 {
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             int N = scan.nextInt();
             solution(N);
         }
+
         public static void solution(int N) {
             List<Integer> answer = new ArrayList<>();
-            int nv = 1;
-            int nf = 0;
+            int res = 1;
+            int des = 0;
             for (int i = 0; i < N; i++) {
-                answer.add(nv);
-                int nt = nv;
-                nv = nv + nf;
-                nf = nt;
+                answer.add(res);
+                int temp = res;
+                res = res + des;
+                des = temp;
             }
             for (int x : answer) {
                 System.out.print(x + " ");
             }
         }
-    }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int[] answer = solution(N);
-        for (int x : answer) {
-            System.out.print(x + " ");
-        }
-    }
-    public static int[] solution(int N) {
-        List<Integer> answer = new ArrayList<>();
-        int prev = 0;
-        int next = 1;
-        for (int i = 0; i < N; i++) {
-            answer.add(next);
-            int temp = next;
-            next = prev + next;
-            prev = temp;
-        }
-        return answer.stream().mapToInt(e -> e).toArray();
     }
 }
