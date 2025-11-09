ec92cb8ce0be6d61a4af520cbfb9741e0f7603ce
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-11-09 13:31:06 +0900

    Feat: 백준 11720 숫자의 합 구하기
    Feat: 백준 11720 숫자의 합 구하기

---
+++ src/do_it_알고리즘_코딩테스트_with_java/자료구조/백준_11720_숫자의_합_구하기.java
@@ -0,0 +1,16 @@
+package do_it_알고리즘_코딩테스트_with_java.자료구조;
+
+import java.util.*;
+
+public class 백준_11720_숫자의_합_구하기 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        String str = scan.next();
+        int sum = 0;
+        for(int i = 0; i < N; i++) {
+            sum += Integer.valueOf(String.valueOf(str.charAt(i)));
+        }
+        System.out.println(sum);
+    }
+}
