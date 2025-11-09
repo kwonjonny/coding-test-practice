ac86b92d8d3a183c003bbe3cf3b97b97dec2d364
권성준 <128343377+kwonjonny@users.noreply.github.com>
2025-11-09 13:48:38 +0900

    Feat: 클래스명 변경
    Feat: 클래스명 변경

--- src/do_it_알고리즘_코딩테스트_with_java/자료구조/A_백준_11720_숫자의_합_구하기.java
+++ src/do_it_알고리즘_코딩테스트_with_java/자료구조/A_백준_11720_숫자의_합_구하기.java
@@ -2,7 +2,7 @@ package do_it_알고리즘_코딩테스트_with_java.자료구조;
 
 import java.util.*;
 
-public class 백준_11720_숫자의_합_구하기 {
+public class A_백준_11720_숫자의_합_구하기 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
